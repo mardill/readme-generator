b7c45cdd4901392df761eb7747c90dd1afe471bf
Mary Dillon <mary.dillon09@gmail.com>
2022-07-16 14:00:38 -0700

    initial files

---
+++ index.js
@@ -0,0 +1,17 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your project name?',
+        },
+        {
+            
+        }
+
+
+
+    ])
