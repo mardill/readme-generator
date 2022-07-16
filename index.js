4851768ca83e4b1aa29c55be6de9edb3f1ad79da
Mary Dillon <mary.dillon09@gmail.com>
2022-07-16 14:41:15 -0700

    starter code for readme file

--- index.js
+++ index.js
@@ -13,36 +13,36 @@ inquirer
             message: 'What is the description of your project?',
             name: 'description'
         },
-        {
-            type: 'input',
-            message: 'How do you install your project?',
-            name: 'installation'
-        },
-        {
-            type: 'input',
-            message: 'What is the usage information?',
-            name: 'usage'
-        },
-        {
-            type: 'input',
-            message: 'Who contributed to your project?',
-            name: 'contributing'
-        },
-        {
-            type: 'input',
-            message: 'What are the text instructions?',
-            name: 'tests'
-        },
-        {
-            type: 'input',
-            message: 'What is your github username?',
-            name: 'github'
-        },
-        {
-            type: 'input',
-            message: 'What is your email?',
-            name: 'email'
-        }
+        // {
+        //     type: 'input',
+        //     message: 'How do you install your project?',
+        //     name: 'installation'
+        // },
+        // {
+        //     type: 'input',
+        //     message: 'What is the usage information?',
+        //     name: 'usage'
+        // },
+        // {
+        //     type: 'input',
+        //     message: 'Who contributed to your project?',
+        //     name: 'contributing'
+        // },
+        // {
+        //     type: 'input',
+        //     message: 'What are the text instructions?',
+        //     name: 'tests'
+        // },
+        // {
+        //     type: 'input',
+        //     message: 'What is your github username?',
+        //     name: 'github'
+        // },
+        // {
+        //     type: 'input',
+        //     message: 'What is your email?',
+        //     name: 'email'
+        // }
     ])
     .then((responses) => {
         const readMeContent = createReadMe(responses);
@@ -51,3 +51,22 @@ inquirer
             err ? console.log(err) : console.log('Success')
         )
     })
+
+function createReadMe(responses) {
+    return
+
+    `
+    # ${resonses.title}
+
+    ## Description
+
+    ${responses.description}
+    
+    `
+}
+    
+
+    
+    `
+
+
